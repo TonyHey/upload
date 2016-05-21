66507e57add76488c48585998bf3b96c4b17e1ad
Tony He <562359635@qq.com>
2016-05-22 00:09:32 +0800

    ADD UPLOAD
    ADD UPLOAD

---
+++ upload/upload.php
@@ -0,0 +1,15 @@
+<?php
+
+$s = "Name:" . $_FILES["image_file"]["name"]."<br />";
+$s .= "Type: " . $_FILES["image_file"]["type"] . "<br />";
+$s .= "Size: " . $_FILES["image_file"]["size"] / 1024 . " Kb<br />";
+echo $s;
+
+if (file_exists("upload/" . $_FILES["image_file"]["name"])) {
+    	echo $_FILES["image_file"]["name"] . " already exists. ";
+    } else {
+    	move_uploaded_file($_FILES["image_file"]["tmp_name"],"upload/" . $_FILES["image_file"]["name"]);
+      	echo "Stored in: " . "upload/" . $_FILES["image_file"]["name"];
+    }
+
+?>
